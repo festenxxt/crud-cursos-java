1c5da29e236baf0da3ef06db4faf828d6e1a1f17
Matheus Mendes <gynmatheus@hotmail.com.br>
2023-11-16 12:13:30 -0300

    Primeiro commit

---
+++ src/main/java/com/matheus/crudcursosjava/CrudCursosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.crudcursosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCursosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCursosJavaApplication.class, args);
+	}
+
+}
